920140e36433e4d9f22e5af76d01bc2cdf0691e5
due <68518940+dueip@users.noreply.github.com>
2022-10-11 11:58:26 +0300

    Update 0136A.cpp

--- voevodin_e_o/0136A.cpp
+++ voevodin_e_o/0136A.cpp
@@ -1,24 +1,24 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//int main() {
-//	size_t size = 0;
-//	std::cin >> size;
-//	std::vector<std::pair<size_t, size_t>> numbers(size);
-//	for (auto& el : numbers) {
-//		std::cin >> el.first;
-//	}
-//
-//	for (size_t i = 0; i < numbers.size(); ++i) {
-//		numbers[i].second = i + 1;
-//	}
-//
-//	
-//	std::sort(numbers.begin(), numbers.end(), [](const std::pair<size_t, size_t>& a, const std::pair<size_t, size_t>& b) {
-//		return a.first < b.first;
-//		});
-//	for (auto& el : numbers) {
-//		std::cout << el.second << " ";
-//	}
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+	size_t size = 0;
+	std::cin >> size;
+	std::vector<std::pair<size_t, size_t>> numbers(size);
+	for (auto& el : numbers) {
+		std::cin >> el.first;
+	}
+
+	for (size_t i = 0; i < numbers.size(); ++i) {
+		numbers[i].second = i + 1;
+	}
+
+	
+	std::sort(numbers.begin(), numbers.end(), [](const std::pair<size_t, size_t>& a, const std::pair<size_t, size_t>& b) {
+		return a.first < b.first;
+		});
+	for (auto& el : numbers) {
+		std::cout << el.second << " ";
+	}
+}
