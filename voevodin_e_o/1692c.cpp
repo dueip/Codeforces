a8dad0162cc1f0b168b519887c540b32c850787f
due <68518940+dueip@users.noreply.github.com>
2022-10-13 22:31:07 +0300

    yeah

--- voevodin_e_o/1692c.cpp
+++ voevodin_e_o/1692c.cpp
@@ -1,39 +1,76 @@
+
 #include <iostream>
+#include <string>
 #include <vector>
+#include <array>
+
+
+// iloveslim = s.find('s');
+// ilovseslim
+// o e
+// j11b11l 
+// ba11oz1
+// s i
+void ban(std::array<bool, 256>& a, char banned) {
+	a[banned] = true;
+}
+
+bool [[nodiscard]] is_banned(std::array<bool, 256>& a, const char& to_check) {
+	return a[to_check];
+}
+
 int main() {
 	size_t t = 0;
 	std::cin >> t;
-	for (t; t > 0; --t) {
-		std::vector<std::pair<size_t, size_t>> possible_candidates;
-		for (size_t y = 0; y < 8; ++y) {
-			bool was_attacked_at_this_already = false;
-			bool was_attacked_twice = false;
-			size_t possible_x = 0;
-			for (size_t x = 0; x < 8; ++x) {
-				char a;
-				bool was_attacked = false;
+	for (size_t j = 0; j < t; ++j) {
+		std::string input;
+		size_t input_size;
+		std::cin >> input_size;
+		std::cin >> input;
+		size_t special_characters_size;
+		std::cin >> special_characters_size;
+		std::vector<char> special_characters;
+		special_characters.resize(special_characters_size);
+		std::array<bool, 256> characters_banned{};
+		
+		for (auto& el : special_characters) {
+			std::cin >> el;
+		}
+		{
+			//size_t i = 0;
+			for (auto& el : special_characters) {
+				ban(characters_banned, el);
 				
-				std::cin >> a;
-				was_attacked = (a == '#');
-
-				
-				if (was_attacked) {
-					was_attacked_at_this_already = true;
-					possible_x = x;
-				}
-				else if (was_attacked && was_attacked_at_this_already) {
-					was_attacked_twice = true;
-					possible_x = 0;
+			}
+		}
+		
+		{
+			size_t i = 0;
+			bool found = false;
+			for (const auto& el : characters_banned) {
+				if (el) {
+					found = true;
 					break;
 				}
+				++i;
 			}
-			if (y != 0 && y!= 7 && possible_x != 0 && possible_x != 7 && !was_attacked_twice) {
-				possible_candidates.push_back(std::pair<size_t, size_t>(y + 1, possible_x + 1));
+			if (!found) {
+				input = "";
 			}
-		}
-		for (auto& el : possible_candidates) {
-			std::cout << el.first << " " << el.second << '\n';
 		}
 
+		size_t counter = 0;
+		size_t max_counter = 0;
+		for (const char& el : input) {
+			if (!is_banned(characters_banned, el)) {
+				++counter;
+			}
+			else {
+				max_counter = std::max(counter, max_counter);
+				counter = 1;
+			}
+
+		}
+		std::cout << max_counter << "\n";
 	}
 }
