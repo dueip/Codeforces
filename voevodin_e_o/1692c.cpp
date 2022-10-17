8ed7c78d6ff3d692bd8762c5e1a003cc25d6070c
due <68518940+dueip@users.noreply.github.com>
2022-10-18 00:36:15 +0300

    Update 1692c.cpp

--- voevodin_e_o/1692c.cpp
+++ voevodin_e_o/1692c.cpp
@@ -1,76 +1,44 @@
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <array>
-
-
-// iloveslim = s.find('s');
-// ilovseslim
-// o e
-// j11b11l 
-// ba11oz1
-// s i
-void ban(std::array<bool, 256>& a, char banned) {
-	a[banned] = true;
-}
-
-bool [[nodiscard]] is_banned(std::array<bool, 256>& a, const char& to_check) {
-	return a[to_check];
-}
-
-int main() {
-	size_t t = 0;
-	std::cin >> t;
-	for (size_t j = 0; j < t; ++j) {
-		std::string input;
-		size_t input_size;
-		std::cin >> input_size;
-		std::cin >> input;
-		size_t special_characters_size;
-		std::cin >> special_characters_size;
-		std::vector<char> special_characters;
-		special_characters.resize(special_characters_size);
-		std::array<bool, 256> characters_banned{};
-		
-		for (auto& el : special_characters) {
-			std::cin >> el;
-		}
-		{
-			//size_t i = 0;
-			for (auto& el : special_characters) {
-				ban(characters_banned, el);
-				
-			}
-		}
-		
-		{
-			size_t i = 0;
-			bool found = false;
-			for (const auto& el : characters_banned) {
-				if (el) {
-					found = true;
-					break;
-				}
-				++i;
-			}
-			if (!found) {
-				input = "";
-			}
-		}
-
-		size_t counter = 0;
-		size_t max_counter = 0;
-		for (const char& el : input) {
-			if (!is_banned(characters_banned, el)) {
-				++counter;
-			}
-			else {
-				max_counter = std::max(counter, max_counter);
-				counter = 1;
-			}
-
-		}
-		std::cout << max_counter << "\n";
-	}
-}
+    #include <iostream>
+    #include <optional>
+     
+    constexpr unsigned char max_lines = 8;
+    constexpr unsigned char max_columns = 8;
+    int main() {
+    	size_t t = 0;
+    	std::cin >> t;
+    	for (t; t > 0; --t) {
+    		std::optional<int8_t> answer_x;
+    		std::optional<int8_t> answer_y;
+    		bool next_one_is_an_answer = false;
+    		for (size_t y = 0; y < max_columns; ++y) {
+    			
+    			std::optional<int8_t> first_x;
+    			std::optional<int8_t> second_x;
+    			for (size_t x = 0; x < max_lines; ++x) {
+    				bool was_hit = false;
+    				
+    				char a;
+    				std::cin >> a;
+    				if (!answer_x.has_value() && !answer_y.has_value()) {
+    					was_hit = (a == '#');
+    					if (was_hit && !first_x.has_value()) {
+    						first_x = x;
+    					}
+    					else if (was_hit && !second_x.has_value()) {
+    						second_x = x;
+    					}
+    					if (second_x.has_value() && second_x.value() - first_x.value() == 2) {
+    						next_one_is_an_answer = true;
+    					}
+     
+    					if (next_one_is_an_answer && first_x.has_value() && second_x.has_value()) {
+    						answer_y = y + 1;
+    						answer_x = first_x.value() + 1;
+    					}
+    				}
+    			}
+    		}
+    		if (answer_y.has_value() && answer_x.has_value())
+    			std::cout << static_cast<int16_t>(answer_y.value()) + 1  << " " << static_cast<int16_t>(answer_x.value()) + 1  << "\n";
+    	}
+    }
